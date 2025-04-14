38a39aad02f6fff716a89322dc269893a5844451
DenysVaschenko <denvoser@gmail.com>
2025-04-15 00:09:45 +0300

    now server is configured, provided with certificate and uses tsl

--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(7161, listenOptions =>
+    {
+        listenOptions.UseHttps("Resources/localhost.p12", "changeit");
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
